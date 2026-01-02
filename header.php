87a760dc0cbdfb5c4dfe93ad961e7c78fba5d8ce
Neree Van der woerdt <nereevanderwoerdt@MBP-van-Neree.fritz.box>
2026-01-02 22:34:53 +0100

    Auto-update: 2026-01-02 22:34:53

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     
     <?php // Open Graph & Twitter Cards ?>
     <meta property="og:site_name" content="<?php echo esc_attr(get_bloginfo('name')); ?>">
